149b3dc23f3fbbcbc43acf80b3a756432fa29fa0
Artem Povaliaev <gevernus@gmail.com>
2020-02-01 15:12:28 +0300

    Fixed bug with raycast another tab

--- Assets/Scripts/Inventory/TabSelector.cs
+++ Assets/Scripts/Inventory/TabSelector.cs
@@ -1,21 +1,22 @@
 using System;
+using Inventory;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class TabSelector : MonoBehaviour {
     public int backLayer;
     public int frontLayer;
-    public SortingGroup selected;
+    public TabController selected;
 
     private void Start() {
         if (selected) {
-            selected.sortingOrder = frontLayer;
+            selected.Select(frontLayer);
         }
     }
 
-    public void Select(SortingGroup sortingGroup) {
-        if (selected) selected.sortingOrder = backLayer;
-        selected = sortingGroup;
-        selected.sortingOrder = frontLayer;
+    public void Select(TabController tabController) {
+        if (selected) selected.UnSelect(backLayer);
+        selected = tabController;
+        selected.Select(frontLayer);
     }
 }
