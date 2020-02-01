de42d9e12c14bab56bb537d684eb660cf9d08c95
Artem Povaliaev <gevernus@gmail.com>
2020-02-01 15:52:49 +0300

    Added tab hotkeys

--- Assets/Scripts/Inventory/TabSelector.cs
+++ Assets/Scripts/Inventory/TabSelector.cs
@@ -7,6 +7,7 @@ public class TabSelector : MonoBehaviour {
     public int backLayer;
     public int frontLayer;
     public TabController selected;
+    public TabController[] tabs;
 
     private void Start() {
         if (selected) {
@@ -18,5 +19,19 @@ public class TabSelector : MonoBehaviour {
         if (selected) selected.UnSelect(backLayer);
         selected = tabController;
         selected.Select(frontLayer);
+    }
+
+    public void Select(KeyCode key) {
+        switch (key) {
+            case KeyCode.Alpha1:
+                Select(tabs[0]);
+                break;
+            case KeyCode.Alpha2:
+                Select(tabs[1]);
+                break;
+            case KeyCode.Alpha3:
+                Select(tabs[2]);
+                break;
+        }
     }
 }
